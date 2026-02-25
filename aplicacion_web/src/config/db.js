09f777ba666531d1e8c16008d3b78edb6eccc95b
HEctor506 <164930438+HEctor506@users.noreply.github.com>
2026-02-25 15:57:30 -0500

    update

--- aplicacion_web/src/config/db.js
+++ aplicacion_web/src/config/db.js
@@ -7,7 +7,7 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT), // 👈 IMPORTANTE
+  port: Number(process.env.DB_PORT), 
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
